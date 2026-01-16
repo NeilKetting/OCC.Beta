1990d1bada3c559e80a105f7b1b2bef5a68cd003
Neil <neil@origize63.co.za>
2026-01-16 15:10:01 +0200

    Db Update

---
+++ OCC.API/Migrations/20260116130712_AddPriceToInventoryItems.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OCC.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToInventoryItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("02df5455-fd85-465a-94b0-3e2afc6c5147"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("10f8d366-9d16-4385-8ec1-871fc54640c1"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("18f5c93a-cf8a-44c5-ad74-aaf1518ebcf8"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("49ca9b35-a6b6-4498-8fa7-41c90281de50"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("5fba8cf1-89df-47a8-a909-d3bb84d2728a"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("789a5d55-fe1b-4683-9901-08c8d7f750f7"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("9ef1597f-298d-425b-b869-ecc39f3863e3"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("d9bf990a-cea4-4e9f-8642-a7e446c80f46"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("e4a81a4b-a8ac-46f0-9335-89cf619d84f8"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("e71fab50-0bcf-44ee-b1fb-6a4ef62899e1"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("ed02e768-3cab-4b75-b4d6-abb6fe794b29"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("f3b46995-5193-484d-80d7-5750d5263554"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("fe2b0451-c692-482b-93c4-b0d1e39e91fd"));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "InventoryItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.InsertData(
+                table: "PublicHolidays",
+                columns: new[] { "Id", "Date", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("09f69d9c-19da-4b15-83b0-1129ba86dbb8"), new DateTime(2026, 3, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Human Rights Day" },
+                    { new Guid("4b7e312a-c120-4945-97b5-353bfaf81ddc"), new DateTime(2026, 6, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Youth Day" },
+                    { new Guid("4f716152-72c5-4c00-9671-77ba451cb751"), new DateTime(2026, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Christmas Day" },
+                    { new Guid("68f29ee1-266e-4221-befd-8dd7564a5a52"), new DateTime(2026, 9, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), "Heritage Day" },
+                    { new Guid("710fca80-08d0-4e02-838b-c7305c591f1e"), new DateTime(2026, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Public Holiday" },
+                    { new Guid("9befaf94-85d5-4902-83f3-320d0dc4dca7"), new DateTime(2026, 8, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "National Women's Day" },
+                    { new Guid("9f11a3ce-e775-4781-a847-53901cd9d237"), new DateTime(2026, 12, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Day of Reconciliation" },
+                    { new Guid("a50a273c-6807-4b36-a02d-f5bfee088392"), new DateTime(2026, 4, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Good Friday" },
+                    { new Guid("a65144b5-b482-4448-8d43-f0fb0f0b5382"), new DateTime(2026, 4, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Family Day" },
+                    { new Guid("b67a528f-9b62-4d1d-984a-bb965b73fdba"), new DateTime(2026, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Workers' Day" },
+                    { new Guid("d28cb84e-6e0e-4c99-b520-916b429c64ff"), new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "New Year's Day" },
+                    { new Guid("f1001f22-4757-426a-bbcf-a1e82aa2f255"), new DateTime(2026, 4, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), "Freedom Day" },
+                    { new Guid("f8542431-06bd-46f4-b36e-6a3696c4222a"), new DateTime(2026, 12, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "Day of Goodwill" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("09f69d9c-19da-4b15-83b0-1129ba86dbb8"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("4b7e312a-c120-4945-97b5-353bfaf81ddc"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("4f716152-72c5-4c00-9671-77ba451cb751"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("68f29ee1-266e-4221-befd-8dd7564a5a52"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("710fca80-08d0-4e02-838b-c7305c591f1e"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("9befaf94-85d5-4902-83f3-320d0dc4dca7"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("9f11a3ce-e775-4781-a847-53901cd9d237"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("a50a273c-6807-4b36-a02d-f5bfee088392"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("a65144b5-b482-4448-8d43-f0fb0f0b5382"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("b67a528f-9b62-4d1d-984a-bb965b73fdba"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("d28cb84e-6e0e-4c99-b520-916b429c64ff"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("f1001f22-4757-426a-bbcf-a1e82aa2f255"));
+
+            migrationBuilder.DeleteData(
+                table: "PublicHolidays",
+                keyColumn: "Id",
+                keyValue: new Guid("f8542431-06bd-46f4-b36e-6a3696c4222a"));
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "InventoryItems");
+
+            migrationBuilder.InsertData(
+                table: "PublicHolidays",
+                columns: new[] { "Id", "Date", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("02df5455-fd85-465a-94b0-3e2afc6c5147"), new DateTime(2026, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Public Holiday" },
+                    { new Guid("10f8d366-9d16-4385-8ec1-871fc54640c1"), new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "New Year's Day" },
+                    { new Guid("18f5c93a-cf8a-44c5-ad74-aaf1518ebcf8"), new DateTime(2026, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Christmas Day" },
+                    { new Guid("49ca9b35-a6b6-4498-8fa7-41c90281de50"), new DateTime(2026, 8, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "National Women's Day" },
+                    { new Guid("5fba8cf1-89df-47a8-a909-d3bb84d2728a"), new DateTime(2026, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Workers' Day" },
+                    { new Guid("789a5d55-fe1b-4683-9901-08c8d7f750f7"), new DateTime(2026, 3, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Human Rights Day" },
+                    { new Guid("9ef1597f-298d-425b-b869-ecc39f3863e3"), new DateTime(2026, 4, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Good Friday" },
+                    { new Guid("d9bf990a-cea4-4e9f-8642-a7e446c80f46"), new DateTime(2026, 12, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "Day of Goodwill" },
+                    { new Guid("e4a81a4b-a8ac-46f0-9335-89cf619d84f8"), new DateTime(2026, 4, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), "Freedom Day" },
+                    { new Guid("e71fab50-0bcf-44ee-b1fb-6a4ef62899e1"), new DateTime(2026, 9, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), "Heritage Day" },
+                    { new Guid("ed02e768-3cab-4b75-b4d6-abb6fe794b29"), new DateTime(2026, 6, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Youth Day" },
+                    { new Guid("f3b46995-5193-484d-80d7-5750d5263554"), new DateTime(2026, 12, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Day of Reconciliation" },
+                    { new Guid("fe2b0451-c692-482b-93c4-b0d1e39e91fd"), new DateTime(2026, 4, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Family Day" }
+                });
+        }
+    }
+}
